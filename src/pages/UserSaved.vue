0d52459a8afaff1981b83360d9884467c664b73b
rahman-anz <rahmanctanas@gmail.com>
2023-06-05 13:13:19 +0530

    Added Netlify Redirection Fix & Favicon

--- src/pages/UserSaved.vue
+++ src/pages/UserSaved.vue
@@ -1,3 +1,14 @@
 <template>
-  <h2>Saved Recipes</h2>
+  <section>
+    <h2>Saved Recipes</h2>
+  </section>
 </template>
+<style scoped>
+section {
+  width: 120rem;
+  margin: 3rem auto;
+}
+h2 {
+  font-size: 3rem;
+}
+</style>
